a194aaa84d03a1c69c03e83344fb3b190aa972a1
Riley <vishnukey@gmail.com>
2020-05-30 03:22:12 -0600

    made NoJuice scene with player shooting and target dummies to be shot

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -2,45 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(CharacterController))]
 public class Player : MonoBehaviour
 {
-    private CharacterController character;
-    private float xRot;
-
-    [SerializeField] private Camera mainCam;
-
-    [SerializeField] private float speed = 1f;
-
-    [SerializeField] private float sensitivity = 100f;
-
+    [SerializeField] private Transform hand;
+    [SerializeField] private Rigidbody projectile;
+    [SerializeField] private float gunStrength = 500f;
+    [SerializeField] private float shotTTL = 5f;
+    
     // Start is called before the first frame update
     void Start()
     {
-        character = GetComponent<CharacterController>();
-        Cursor.lockState = CursorLockMode.Locked;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        var moveDir = (
-                Input.GetAxis("Vertical") * transform.forward + 
-                Input.GetAxis("Horizontal") * transform.right
-            ).normalized;
-        
-        var mouse = new Vector2(
-                Input.GetAxis("Mouse X"),
-                Input.GetAxis("Mouse Y")
-            ) * (sensitivity * Time.deltaTime);
+        if (Input.GetMouseButtonDown(0)) Shoot();
+    }
 
-        xRot -= mouse.y;
-        xRot = Mathf.Clamp(xRot, -90f, 90f);
-        
-        mainCam.transform.localRotation = Quaternion.Euler(xRot, 0f, 0f);
-        transform.Rotate(Vector3.up, mouse.x);
-        character.Move(moveDir * (speed * Time.deltaTime));
-
-        if (Input.GetKey(KeyCode.Escape)) Cursor.lockState = CursorLockMode.None;
+    private void Shoot()
+    {
+        var shot = Instantiate(projectile, hand.position, hand.rotation);
+        shot.AddForce(hand.forward * gunStrength);
+        Destroy(shot.gameObject, shotTTL);
     }
 }
---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController character;
+    private float xRot;
+
+    [SerializeField] private Camera mainCam;
+
+    [SerializeField] private float speed = 1f;
+
+    [SerializeField] private float sensitivity = 100f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        character = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var moveDir = (
+                Input.GetAxis("Vertical") * transform.forward + 
+                Input.GetAxis("Horizontal") * transform.right
+            ).normalized;
+        
+        var mouse = new Vector2(
+                Input.GetAxis("Mouse X"),
+                Input.GetAxis("Mouse Y")
+            ) * (sensitivity * Time.deltaTime);
+
+        xRot -= mouse.y;
+        xRot = Mathf.Clamp(xRot, -90f, 90f);
+        
+        mainCam.transform.localRotation = Quaternion.Euler(xRot, 0f, 0f);
+        transform.Rotate(Vector3.up, mouse.x);
+        character.Move(moveDir * (speed * Time.deltaTime));
+
+        if (Input.GetKey(KeyCode.Escape)) Cursor.lockState = CursorLockMode.None;
+    }
+}
